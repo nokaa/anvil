2b4a89106b955036ad9d8bf4709afe0a1a1eb4b7
nokaa <nokaa@cock.li>
2016-01-29 04:08:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Forge
+Forge is my experiment with text editors. I wanted to see what makes a text editor a text editor.
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+extern crate clap;
+
+use clap::App;
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
+use std::path::Path;
+
+fn main() {
+    // Clap handles command line args for us.
+    let matches = App::new("forge")
+                      .version("0.1")
+                      .author("nokaa <nokaa@cock.li>")
+                      .about("A text editor")
+                      .arg_from_usage("[OUTPUT] 'Sets the output file to use'")
+                      .get_matches();
+
+    // For now we are just reading from stdin, so we check to see if the user passed a file to
+    // output to. Otherwise we output to stdout
+    if let Some(file) = matches.value_of("OUTPUT") {
+        let mut input = Vec::new();
+        if let Err(e) = io::stdin().read_to_end(&mut input) {
+            panic!("Error: {}", e);
+        }
+
+        if let Err(e) = write_file(file.to_string(), input) {
+            panic!("Error: {}", e);
+        }
+    } else {
+        let mut input = Vec::new();
+        if let Err(e) = io::stdin().read_to_end(&mut input) {
+            panic!("Error: {}", e);
+        }
+        if let Err(e) = io::stdout().write_all(input.as_slice()) {
+            panic!("Error: {}", e);
+        }
+    }
+}
+
+#[allow(dead_code)]
+/// Helper function for reading from a file. Reads from `filename` and returns a `Vec<u8>`.
+fn read_file(filename: String) -> Vec<u8> {
+    let mut reader = File::open(Path::new(&filename)).ok().expect("Unable to open file");
+    let mut buf: Vec<u8> = vec![];
+    reader.read_to_end(&mut buf).unwrap();
+
+    buf
+}
+
+/// Helper function for writing to a file. Writes `contents` to `filename`.
+fn write_file(filename: String, contents: Vec<u8>) -> Result<(), std::io::Error> {
+    let mut file = try!(File::create(Path::new(&filename)));
+    try!(file.write_all(contents.as_slice()));
+    Ok(())
+}
