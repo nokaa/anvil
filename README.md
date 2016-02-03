bee25dfd59fba0eed0df1e08f0b861153a1c4da1
nokaa <nokaa@cock.li>
2016-02-03 03:54:33 -0600

    Forge runs a TCP server now

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Forge is my experiment with text editors. I wanted to see what makes a text editor a text editor.
 
 [![Build Status](https://travis-ci.org/nokaa/forge.svg?branch=master)](https://travis-ci.org/nokaa/forge)
+
+At this time Forge launches a TCP server at `0.0.0.0:3000` and listens for input. Upon receiving input, it writes the input to a file `test` in the current directory.
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,61 @@
 extern crate clap;
-extern crate mio;
+extern crate rotor;
 
 use clap::App;
-use mio::*;
+use rotor::{EventSet, PollOpt, Loop, Config};
+use rotor::mio::{TryRead, TryWrite};
+use rotor::mio::tcp::{TcpListener, TcpStream};
+use rotor::{Machine, Response, EarlyScope, Scope};
+
+use std::error::Error;
 use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
+use std::net::SocketAddr;
 use std::path::Path;
 
 fn main() {
     // Clap handles command line args for us.
-    let matches = App::new("forge")
-                      .version("0.1")
-                      .author("nokaa <nokaa@cock.li>")
-                      .about("A text editor")
-                      .arg_from_usage("[OUTPUT] 'Sets the output file to use'")
-                      .get_matches();
+    let _matches = App::new("forge")
+                       .version("0.1")
+                       .author("nokaa <nokaa@cock.li>")
+                       .about("A text editor")
+                       .arg_from_usage("[OUTPUT] 'Sets the output file to use'")
+                       .get_matches();
+
+    run("0.0.0.0:3000".parse().unwrap());
 
     // For now we are just reading from stdin, so we check to see if the user passed a file to
     // output to. Otherwise we output to stdout
-    if let Some(file) = matches.value_of("OUTPUT") {
-        let mut input = Vec::new();
-        if let Err(e) = io::stdin().read_to_end(&mut input) {
-            panic!("Error: {}", e);
-        }
+    //    if let Some(file) = matches.value_of("OUTPUT") {
+    // let mut input = Vec::new();
+    // if let Err(e) = io::stdin().read_to_end(&mut input) {
+    // panic!("Error: {}", e);
+    // }
+    //
+    // if let Err(e) = write_file(file.to_string(), input) {
+    // panic!("Error: {}", e);
+    // }
+    // } else {
+    // let mut input = Vec::new();
+    // if let Err(e) = io::stdin().read_to_end(&mut input) {
+    // panic!("Error: {}", e);
+    // }
+    // if let Err(e) = io::stdout().write_all(&input[..]) {
+    // panic!("Error: {}", e);
+    // }
+    // }
 
-        if let Err(e) = write_file(file.to_string(), input) {
-            panic!("Error: {}", e);
-        }
-    } else {
-        let mut input = Vec::new();
-        if let Err(e) = io::stdin().read_to_end(&mut input) {
-            panic!("Error: {}", e);
-        }
-        if let Err(e) = io::stdout().write_all(&input[..]) {
-            panic!("Error: {}", e);
-        }
-    }
+}
+
+/// This function takes a SocketAddr and starts the rotor event loop
+/// with on this address.
+fn run(address: SocketAddr) {
+    let mut loop_creator = Loop::new(&Config::new()).unwrap();
+    let lst = TcpListener::bind(&address).unwrap();
+    loop_creator.add_machine_with(|scope| Ok(Echo::new(lst, scope)))
+                .unwrap();
+    loop_creator.run(Context).unwrap();
 }
 
 #[allow(dead_code)]
@@ -49,9 +68,124 @@ fn read_file(filename: String) -> Vec<u8> {
     buf
 }
 
+#[allow(dead_code)]
 /// Helper function for writing to a file. Writes `contents` to `filename`.
 fn write_file(filename: String, contents: Vec<u8>) -> Result<(), std::io::Error> {
     let mut file = try!(File::create(Path::new(&filename)));
     try!(file.write_all(&contents[..]));
     Ok(())
+}
+
+#[allow(dead_code)]
+/// Helper function for writing to a file. Writes `contents` to `filename`.
+fn write_file_slice(filename: String, contents: &[u8]) -> Result<(), std::io::Error> {
+    let mut file = try!(File::create(Path::new(&filename)));
+    try!(file.write_all(contents));
+    Ok(())
+}
+
+/// Takes a `&[u8]` and returns a `Vec<u8>` containing all values until the first 0.
+fn get_nonzero_bytes(data: &[u8]) -> Vec<u8> {
+    let mut buf: Vec<u8> = vec![];
+    for ch in data {
+        if *ch == 0u8 {
+            break;
+        } else {
+            buf.push(*ch);
+        }
+    }
+
+    buf
+}
+
+struct Context;
+
+enum Echo {
+    Server(TcpListener),
+    Connection(TcpStream),
+}
+
+impl Echo {
+    pub fn new(sock: TcpListener, scope: &mut EarlyScope) -> Echo {
+        scope.register(&sock, EventSet::readable(), PollOpt::edge())
+             .unwrap();
+        Echo::Server(sock)
+    }
+
+    fn accept(self) -> Response<Echo, TcpStream> {
+        match self {
+            Echo::Server(sock) => {
+                match sock.accept() {
+                    Ok(Some((conn, _))) => Response::spawn(Echo::Server(sock), conn),
+                    Ok(None) => Response::ok(Echo::Server(sock)),
+                    Err(e) => {
+                        writeln!(&mut io::stderr(), "Error: {}", e).ok();
+                        Response::ok(Echo::Server(sock))
+                    }
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Machine for Echo {
+    type Context = Context;
+    type Seed = TcpStream;
+
+    fn create(conn: TcpStream, scope: &mut Scope<Context>) -> Result<Self, Box<Error>> {
+        scope.register(&conn, EventSet::readable(), PollOpt::level())
+             .unwrap();
+        Ok(Echo::Connection(conn))
+    }
+
+    fn ready(self, _events: EventSet, _scope: &mut Scope<Context>) -> Response<Self, TcpStream> {
+        match self {
+            me @ Echo::Server(..) => me.accept(),
+            Echo::Connection(mut sock) => {
+                let mut data = [0u8; 1024];
+                match sock.try_read(&mut data) {
+                    Ok(Some(0)) => Response::done(),
+                    Ok(Some(x)) => {
+                        // Take the received data and write to file.
+                        let input = get_nonzero_bytes(&data[..]);
+                        let _ = write_file("test".to_string(), input);
+                        // Write received data to console for testing purposes
+                        let _ = io::stdout().write_all(&data[..]);
+                        match sock.try_write(&data[..x]) {
+                            Ok(_) => {
+                                // This is example so we don't care if not all
+                                // (or none at all) bytes are written
+                                Response::ok(Echo::Connection(sock))
+                            }
+                            Err(e) => {
+                                writeln!(&mut io::stderr(), "write: {}", e).ok();
+                                Response::done()
+                            }
+                        }
+                    }
+                    Ok(None) => Response::ok(Echo::Connection(sock)),
+                    Err(e) => {
+                        writeln!(&mut io::stderr(), "read: {}", e).ok();
+                        Response::done()
+                    }
+                }
+            }
+        }
+    }
+
+    fn spawned(self, _scope: &mut Scope<Context>) -> Response<Self, TcpStream> {
+        match self {
+            me @ Echo::Server(..) => me.accept(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn timeout(self, _scope: &mut Scope<Context>) -> Response<Self, TcpStream> {
+        unreachable!();
+    }
+
+    fn wakeup(self, _scope: &mut Scope<Context>) -> Response<Self, TcpStream> {
+        unreachable!();
+    }
 }
