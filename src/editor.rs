e5823c3cebd576861d85d192708918d9e72eef45
nokaa <nokaa@cock.li>
2016-03-16 12:08:10 -0500

    Added file contents field to editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,9 @@
+use file;
+
 pub struct Editor {
     mode: EditorMode,
     filename: String,
+    contents: Vec<u8>,
 }
 
 #[derive(PartialEq)]
@@ -11,10 +14,22 @@ pub enum EditorMode {
 
 impl Editor {
     pub fn new(filename: String) -> Editor {
+        let contents: Vec<u8>;
+        if filename == "".to_string() {
+            contents = vec![b'F', b'o', b'r', b'g', b'e'];
+        } else {
+            contents = file::read_file(&filename[..]);
+        }
+
         Editor {
             mode: EditorMode::Command,
             filename: filename,
+            contents: contents,
         }
+    }
+
+    pub fn filename(&self) -> String {
+        self.filename.clone()
     }
 
     pub fn command_mode(&self) -> bool {
