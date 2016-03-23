d00ecb99f38daa5c477dd911928d8110206f0a78
nokaa <nokaa@cock.li>
2016-03-23 16:02:30 -0500

    Implement general insertion
    The generic case of insert mode now works as expected.

--- src/term/insert.rs
+++ src/term/insert.rs
@@ -15,23 +15,25 @@ pub fn handle(term: &mut Term, ch: char) {
         '\x7f' => { // Backspace key
             let line = term.current_line() + term.cursor.pos.y;
             term.cursor.save_pos();
+
             if term.cursor.pos.x == 0 {
                 let mut rem = term.editor.contents[line].clone();
                 let pos = term.editor.contents[line - 1].len() - 1;
+
                 term.editor.contents[line - 1].remove(pos);
                 term.editor.contents[line - 1].append(&mut rem);
                 term.editor.contents.remove(line);
+
                 if term.cursor.pos.y != 0 {
                     term.cursor.pos.y -= 1;
                 }
 
-                //let line = term.current_line() + term.cursor.pos.y;
-                //let len = term.editor.contents[line].len() - 1;
                 term.cursor.pos.x = pos;
                 term.redraw_file();
             } else {
                 term.cursor.pos.x -= 1;
                 let pos = term.cursor.pos.x;
+
                 term.editor.contents[line].remove(pos);
                 term.redraw_file();
             }
@@ -39,9 +41,11 @@ pub fn handle(term: &mut Term, ch: char) {
         '\r' => {
             let line = term.current_line() + term.cursor.pos.y;
             term.editor.contents.insert(line, vec![b'\n']);
+
             term.cursor.save_pos();
             term.cursor.pos.x = 0;
             term.cursor.pos.y += 1;
+
             term.redraw_file();
         }
         '\t' => {
@@ -49,16 +53,19 @@ pub fn handle(term: &mut Term, ch: char) {
                 let x = term.cursor.pos.x + i;
                 term.term[(x, term.cursor.pos.y)].set_ch(' ');
             }
+
             term.cursor.save_pos();
             term.cursor.pos.x += 4;
         }
         c @ _ => {
-            /*let line = term.current_line() + term.cursor.pos.y;
+            let line = term.current_line() + term.cursor.pos.y;
             let pos = term.cursor.pos.x;
-            term.editor.contents[line, pos*/
-            term.term[term.cursor.current_pos()].set_ch(c);
+
+            term.editor.contents[line].insert(pos, c as u8);
+
             term.cursor.save_pos();
             term.cursor.pos.x += 1;
+            term.redraw_file();
         }
     }
 }
